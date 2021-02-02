7498811d84c3b9f80c691f800a0ac589a344ac83
hatless-marija <marija@hatless-studios.co.uk>
2021-02-02 14:14:11 +0000

    Add User Interface and Styling for the test runner

--- tp/apps/frontend/views.py
+++ tp/apps/frontend/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Ding Dong")
+    response = {
+        "datasets": [{'name': "UCY(University)"},
+                    {'name': "UCY(Zara)"},
+                    {'name': "ETH 1"},
+                    {'name': "ETH 2"},
+                    {'name':'Stanford Drone'}
+                    ]
+        }
+    return render(request, "index.html", response)
